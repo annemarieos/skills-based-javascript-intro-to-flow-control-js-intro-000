1ceb26d465170af933d7d558a4f376af5069fffc
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-02 15:19:25 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,15 @@
-if basicTeenager(age >= 13 && age <= 19) {
-  console.log('You are a teenager!')
+function squareNumber(x) {
+  x * x
 }
-} else if basicTeenager(age > 13 || age < 19){
-  console.log('You are not a teenager')
-}
+  console.log(squareNumber(5))
 
+function basicTeenager(age) {
+  if (13 <= age <= 19) {
+    console.log('You are a teenager!')
+  } else {
+    console.log('You are not a teenager')
+  }
+} 
 function ageChecker(age) {
 
 }
